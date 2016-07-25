f9fabdbe0ebf04a091c37d136d23125009a03ecd
cuproPanda <cupropanda@gmail.com>
2016-07-25 04:06:39 -0400

    HACK: fixed the quarry issues for now, better solution for A15

--- Source/Quarry/QuarryManager.cs
+++ Source/Quarry/QuarryManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using Verse;
 
@@ -12,11 +13,36 @@ namespace Quarry {
     // Is there currently a quarry spawned?
     public bool Spawned = false;
 
+    // Resolve issues with prior versions
+    private delegate IntVec3 Del_Offset(IntVec3 newLoc);
+    private static IntVec3 offsetUL(IntVec3 basePos) { return basePos + new IntVec3(-3, 0, 3); }
+    private static IntVec3 offsetUR(IntVec3 basePos) { return basePos + new IntVec3(3, 0, 3); }
+    private static IntVec3 offsetLL(IntVec3 basePos) { return basePos + new IntVec3(-3, 0, -3); }
+    private static IntVec3 offsetLR(IntVec3 basePos) { return basePos + new IntVec3(3, 0, -3); }
+
+
     public override void ExposeData() {
       base.ExposeData();
+
       Scribe_References.LookReference(ref Base, "QRY_QuarryManager_Base");
-      Scribe_Collections.LookList(ref Quads, "QRY_QuarryManager_Quads", LookMode.Deep);
       Scribe_Values.LookValue(ref Spawned, "QRY_QuarryManager_Spawned", false);
+
+      // TODO: In A15, replace this with:
+      //Scribe_Collections.LookList(ref Quads, "QRY_QuarryManager_Quads", LookMode.MapReference);
+      // For now, each load has to regenerate the list of quadrants
+      if (Scribe.mode == LoadSaveMode.LoadingVars && Quads == null) {
+        // Setup offsets
+        Del_Offset Del_UL = new Del_Offset(offsetUL);
+        Del_Offset Del_UR = new Del_Offset(offsetUR);
+        Del_Offset Del_LL = new Del_Offset(offsetLL);
+        Del_Offset Del_LR = new Del_Offset(offsetLR);
+
+        // Manually add the quadrants
+        Quads.Add(Del_UL(Base.Position).GetEdifice() as Building_Quarry);
+        Quads.Add(Del_UR(Base.Position).GetEdifice() as Building_Quarry);
+        Quads.Add(Del_LL(Base.Position).GetEdifice() as Building_Quarry);
+        Quads.Add(Del_LR(Base.Position).GetEdifice() as Building_Quarry);
+      }
     }
 
 
