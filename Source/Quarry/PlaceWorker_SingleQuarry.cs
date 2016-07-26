4fb304770b052e5197eb7a0c67ca485b85cd2be4
cuproPanda <cupropanda@gmail.com>
2016-07-26 17:24:42 -0400

    Fixed steam geyser placement bug

--- Source/Quarry/PlaceWorker_SingleQuarry.cs
+++ Source/Quarry/PlaceWorker_SingleQuarry.cs
@@ -1,18 +1,40 @@
+using System.Collections.Generic;
+
+using RimWorld;
 using Verse;
 
 namespace Quarry {
 
   public class PlaceWorker_SingleQuarry : PlaceWorker {
 
+    List<IntVec3> occupiedCellsTemp = new List<IntVec3>();
+
+
     public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot) {
 
       if (Find.Map.GetComponent<QuarryManager>().Spawned) {
         return "QuarryAlreadyBuilt".Translate();
       }
+
       // This prevents placing multiple blueprints to get around the check
       if (Find.ListerThings.ThingsOfDef(ThingDef.Named("QRY_QuarrySpawner").blueprintDef).Count > 0){
         return "QuarryAlreadyBuilt".Translate();
       }
+
+      // Don't allow placing on steam geysers - obviously
+      occupiedCellsTemp.Clear();
+      foreach (IntVec3 current in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size)) {
+        occupiedCellsTemp.Add(current);
+      }
+
+      for (int i = 0; i < occupiedCellsTemp.Count; i++) {
+        IntVec3 c = occupiedCellsTemp[i];
+        Thing geyser = Find.ThingGrid.ThingAt(c, ThingDefOf.SteamGeyser);
+        if (geyser != null) {
+          return false;
+        }
+      }
+
       return true;
     }
   }
