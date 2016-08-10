2701929c1608fbcb2abfa391ab9969de36dc707b
cuproPanda <cupropanda@gmail.com>
2016-08-10 16:59:57 -0400

    Quarry mining has its own workType, quarry spawns with filth

--- Source/Quarry/PlaceWorker_SingleQuarry.cs
+++ Source/Quarry/PlaceWorker_SingleQuarry.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 using RimWorld;
@@ -23,16 +24,32 @@ namespace Quarry {
 
       // Don't allow placing on steam geysers - in dev mode
       occupiedCellsTemp.Clear();
+      int occCells = 0;
+      int rockCells = 0;
       foreach (IntVec3 current in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size)) {
         occupiedCellsTemp.Add(current);
+        occCells++;
       }
 
       for (int i = 0; i < occupiedCellsTemp.Count; i++) {
         IntVec3 c = occupiedCellsTemp[i];
+
+        // Try to find a geyser
         Thing geyser = Find.ThingGrid.ThingAt(c, ThingDefOf.SteamGeyser);
         if (geyser != null) {
           return false;
         }
+
+        // Make sure the quarry is over sufficient rock
+        Predicate<TerrainDef> validator = ((TerrainDef t) => t.defName.EndsWith("_Rough") || t.defName.EndsWith("_RoughHewn") || t.defName.EndsWith("_Smooth") || t == TerrainDefOf.Gravel);
+        if (validator(c.GetTerrain())) {
+          rockCells++;
+        }
+      }
+
+      // Require at least 60% rocky terrain
+      if ((float)(occCells - rockCells) / occCells > 0.4f) {
+        return false;
       }
 
       return true;
